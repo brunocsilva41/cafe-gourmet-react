d229f7d617c481c60e0eb984c92603596eb1dd96
Bruno Silva <84159328+brunocsilva41@users.noreply.github.com>
2024-11-08 20:57:17 -0300

    fix --

--- src/pages/produtos.jsx
+++ src/pages/produtos.jsx
@@ -27,7 +27,7 @@ const Products = () => {
   useEffect(() => {
     const fetchProdutos = async () => {
       try {
-        const response = await axios.get(`${base_URL}/api/produtos`);
+        const response = await axios.get(`${base_URL}api/produtos`);
         console.log('Resposta da API:', response.data); // Adicionar log para verificar a resposta da API
         if (response.headers['content-type'].includes('application/json')) { // Verificação ajustada
           const data = response.data;
