0d433c53a7c5bb41ab37179c1fb1465aa9b438d3
Bruno Silva <84159328+brunocsilva41@users.noreply.github.com>
2024-11-13 22:45:12 -0300

    correção login --

--- src/utils/authUtils.js
+++ src/utils/authUtils.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const base_URL = process.env.REACT_APP_BASE_URL;
+const base_URL = `https://${process.env.REACT_APP_BASE_URL}`;
 
 export const useVerificarLogin = () => {
   const navigate = useNavigate();
