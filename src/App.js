585ee8cfd8798f2426f4c33cbe9c4336bbdf7d3d
Bruno Silva <84159328+brunocsilva41@users.noreply.github.com>
2024-11-23 16:06:17 -0300

    removendo rota de confirmação --

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Checkout from './components/checkout';
-import Confirmacao from './components/confirmacao';
 import Criarconta from './components/Criarconta';
 import Login from './components/login';
 import Navbar from './components/Navbar';
@@ -37,7 +36,6 @@ const App = () => {
           <Route path="/cart" element={<Cart />} />
           <Route path="/conta" element={<PrivateRoute><Conta /></PrivateRoute>} />
           <Route path="/checkout" element={<PrivateRoute><Checkout /></PrivateRoute>} />
-          <Route path="/confirmacao" element={<Confirmacao />} />
           <Route path="/login" element={<Login />} />
           <Route path="/criarConta" element={<Criarconta />} />
           <Route path="/admin-dashboard" element={<PrivateRoute><AdminDashboard /></PrivateRoute>} />
