073d6e44a2a06f41cb5c1dce40e19fb3cd62853f
Bruno Silva <84159328+brunocsilva41@users.noreply.github.com>
2024-11-23 13:48:57 -0300

    correção de requisição e melhoria no css --

--- src/services/pedidoService.js
+++ src/services/pedidoService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const criarPedido = async (pedido) => {
   try {
-    const response = await axios.post('/criar-pedido', pedido);
+    const response = await axios.post('https://api-cafe-gourmet.vercel.app/criar-pedido', pedido);
     return response.data;
   } catch (error) {
     throw new Error('Erro ao criar pedido: ' + error.message);
