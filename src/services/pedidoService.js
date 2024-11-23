0caa20a0df86accfe4eb9f8fb9b4646fa0773bdc
Bruno Silva <84159328+brunocsilva41@users.noreply.github.com>
2024-11-23 17:56:26 -0300

    corrigindo requisição dos pedidos --

--- src/services/pedidoService.js
+++ src/services/pedidoService.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-export const criarPedido = async (pedido , userId) => {
-    localStorage.getItem('userId');
-    if (!userId) {
+export const criarPedido = async (pedido, userId) => {
+  localStorage.getItem('userId');
+  if (!userId) {
     throw new Error('userId é obrigatório para criar pedido.');
-    }
-    try {
-    const response = await axios.post('https://api-cafe-gourmet.vercel.app/criar-pedido/${userId}', pedido);
+  }
+  try {
+    const response = await axios.post(`https://api-cafe-gourmet.vercel.app/criar-pedido/${userId}`, pedido);
     return response.data;
   } catch (error) {
     throw new Error('Erro ao criar pedido: ' + error.message);
@@ -14,8 +14,8 @@ export const criarPedido = async (pedido , userId) => {
 };
 
 export const obterPedidos = async (userId) => {
-    localStorage.getItem('userId');
-    if (!userId) {
+  localStorage.getItem('userId');
+  if (!userId) {
     throw new Error('userId é obrigatório para obter pedidos.');
   }
   try {
