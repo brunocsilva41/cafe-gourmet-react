2c5cb29054ac963605efee6bcc6b493f656afb8d
Bruno Silva <84159328+brunocsilva41@users.noreply.github.com>
2024-11-30 14:05:28 -0300

    ajuste no URI de redirecionamento para o login do Google e Facebook

--- src/components/SocialLogin.jsx
+++ src/components/SocialLogin.jsx
@@ -22,7 +22,7 @@ const handleSocialLogin = async (email) => {
 
 const handleGoogleLogin = () => {
   const clientId = '731636636395-dp041m5mii0ma67ueog72b3kei3uspeo.apps.googleusercontent.com';
-  const redirectUri = 'https://coffeforyou.netlify.app/criarconta'; // Certifique-se de que este URI está configurado no console de APIs do Google
+  const redirectUri = 'https://coffeforyou.netlify.app/Criarconta'; // Certifique-se de que este URI está configurado no console de APIs do Google
   const scope = 'email profile';
   const responseType = 'token';
   const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=${responseType}`;
@@ -32,7 +32,7 @@ const handleGoogleLogin = () => {
 
 const handleFacebookLogin = () => {
   const appId = '926133692789023';
-  const redirectUri = 'https://coffeforyou.netlify.app/criarconta';
+  const redirectUri = 'https://coffeforyou.netlify.app/Criarconta';
   const scope = 'email';
   const responseType = 'token';
   const url = `https://www.facebook.com/v10.0/dialog/oauth?client_id=${appId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=${responseType}`;
