d111d3b74a797f96bb27772a919bfd2efc3e58a5
Bruno Silva <84159328+brunocsilva41@users.noreply.github.com>
2024-11-30 17:18:23 -0300

    Ajustando direcionamento apos login social --

--- src/components/SocialLogin.jsx
+++ src/components/SocialLogin.jsx
@@ -27,7 +27,7 @@ const handleSocialLogin = async (email) => {
 
 const handleGoogleLogin = () => {
   const clientId = '731636636395-dp041m5mii0ma67ueog72b3kei3uspeo.apps.googleusercontent.com';
-  const redirectUri = 'https://coffeforyou.netlify.app/Criarconta';
+  const redirectUri = 'https://coffeforyou.netlify.app/conta'; 
   const scope = 'email profile';
   const responseType = 'token';
   const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=${responseType}`;
@@ -37,7 +37,7 @@ const handleGoogleLogin = () => {
 
 const handleFacebookLogin = () => {
   const appId = '926133692789023';
-  const redirectUri = 'https://coffeforyou.netlify.app/Criarconta';
+  const redirectUri = 'https://coffeforyou.netlify.app/conta'; 
   const scope = 'email';
   const responseType = 'token';
   const url = `https://www.facebook.com/v10.0/dialog/oauth?client_id=${appId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=${responseType}`;
