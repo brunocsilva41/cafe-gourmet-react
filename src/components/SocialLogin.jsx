64ec18963309cef58853bdd913833d80fe944a57
Bruno Silva <84159328+brunocsilva41@users.noreply.github.com>
2024-11-30 05:17:22 -0300

    ajustes teste--

--- src/components/SocialLogin.jsx
+++ src/components/SocialLogin.jsx
@@ -25,7 +25,7 @@ const handleSocialResponse = async (email, name) => {
 
 const handleGoogleLogin = () => {
   const clientId = '731636636395-dp041m5mii0ma67ueog72b3kei3uspeo.apps.googleusercontent.com';
-  const redirectUri = 'https://coffeforyou.netlify.app/conta';
+  const redirectUri = '';
   const scope = 'email profile';
   const responseType = 'token';
   const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=${responseType}`;
